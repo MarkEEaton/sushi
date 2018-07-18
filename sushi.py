e460e5433822aabb942f8f796f54725662be56e5
Mark Eaton <Mark.Eaton@kbcc.cuny.edu>
2018-07-18 14:08:18 -0400

    inital commit

---
+++ sushi.py
@@ -0,0 +1,26 @@
+""" a tool to download counter reports """
+import datetime
+import cred
+import pycounter
+
+def main():
+    """ get the reports """
+    data = cred.dbs
+    for report_name in ['JR1', 'DB1']:
+        for item in data:
+            outfile = item['name'] + '-' + report_name + '.tsv'
+            try:
+                report = pycounter.sushi.get_report(wsdl_url=item['wsdl_url'],
+                                                    start_date=datetime.date(2017, 7, 1),
+                                                    end_date=datetime.date(2018, 6, 30),
+                                                    requestor_id=item['requestor_id'],
+                                                    customer_reference=item['customer_reference'],
+                                                    report=report_name,
+                                                    release=item['release'])
+                print(outfile)
+                report.write_tsv(outfile)
+            except:
+                print(outfile + ' : report not found.')
+
+if __name__ == '__main__':
+    main()
