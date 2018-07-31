dc4a55c5c305f7d3887650d6f982077b82f00294
Mark Eaton <Mark.Eaton@kbcc.cuny.edu>
2018-07-31 18:58:28 -0400

    specify desired reports in cred.py rather than sushi.py

--- sushi.py
+++ sushi.py
@@ -6,8 +6,8 @@ import pycounter
 def main():
     """ get the reports """
     data = cred.dbs
-    for report_name in ['JR1', 'DB1']:
-        for item in data:
+    for item in data:
+        for report_name in item['reports']:
             outfile = item['name'] + '-' + report_name + '.tsv'
             try:
                 report = pycounter.sushi.get_report(wsdl_url=item['wsdl_url'],
@@ -15,10 +15,8 @@ def main():
                                                     end_date=datetime.date(2018, 6, 30),
                                                     requestor_id=item['requestor_id'],
                                                     customer_reference=item['customer_reference'],
-                                                    customer_name=item.get('loc'),
                                                     report=report_name,
                                                     release=item['release'])
-                print(outfile)
                 report.write_tsv(outfile)
             except Exception as e:
                 print(outfile + ' : report not found : ' + e.__class__.__name__)
