61b2e8c13ed2543f048c8ca529d9775b087722d4
Mark Eaton <Mark.Eaton@kbcc.cuny.edu>
2018-08-02 16:26:31 -0400

    add reports directory; customizations for Gale

--- sushi.py
+++ sushi.py
@@ -8,16 +8,19 @@ def main():
     data = cred.dbs
     for item in data:
         for report_name in item['reports']:
-            outfile = item['name'] + '-' + report_name + '.tsv'
+            outfile = 'reports/' + item['name'] + '-' + report_name + '.tsv'
             try:
                 report = pycounter.sushi.get_report(wsdl_url=item['wsdl_url'],
                                                     start_date=datetime.date(2017, 7, 1),
                                                     end_date=datetime.date(2018, 6, 30),
                                                     requestor_id=item['requestor_id'],
                                                     customer_reference=item['customer_reference'],
+                                                    customer_name=item.get('customer_name'),
+                                                    release=item['release'],
                                                     report=report_name,
-                                                    release=item['release'])
+                                                    sushi_dump=True)
                 report.write_tsv(outfile)
+                print(outfile)
             except Exception as e:
                 print(outfile + ' : report not found : ' + e.__class__.__name__)
                 raise
