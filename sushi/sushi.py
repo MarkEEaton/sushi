640dca05fb52541a72c1025245ff5067a3d522a0
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2019-05-27 21:38:53 -0400

    warn on missing dates

--- sushi/sushi.py
+++ sushi/sushi.py
@@ -4,6 +4,7 @@ import logging
 import pycounter
 import cred
 import argparse
+from dateutil.rrule import rrule, MONTHLY
 
 logging.basicConfig(level=logging.DEBUG, filename="debug.log", filemode="w")
 
@@ -15,7 +16,7 @@ def main(data, directory):
     """ get the reports """
     for item in data:
 
-        custom = '\033[93m'
+        custom = "\033[93m"
         # use specified dates or default dates
         if item.get("custom_start_date"):
             start_date = item["custom_start_date"]
@@ -24,13 +25,17 @@ def main(data, directory):
             start_date = default_start_date
         if item.get("custom_end_date"):
             end_date = item["custom_end_date"]
-            custom = custom + "Custom end date. "
+            custom = custom + "Custom end date."
         else:
             end_date = default_end_date
-        custom = custom + '\033[0m'
+        custom = custom + "\033[0m"
+
+        date_list = [dt for dt in rrule(MONTHLY, dtstart=start_date, until=end_date)]
+        formatted_date = [d.strftime("%b-%Y") for d in date_list]
 
         # run the reports
         for report_name in item["reports"]:
+            warning = ""
             outfile = directory + item["name"] + "-" + report_name + ".tsv"
             try:
                 report = pycounter.sushi.get_report(
@@ -44,8 +49,18 @@ def main(data, directory):
                     report=report_name,
                     sushi_dump=True,
                 )
+
+                # check that all of the dates are present in the table
+                table = report.as_generic()
+                for d in formatted_date:
+                    if d in table[7]:
+                        pass
+                    else:
+                        warning = "\033[91mDates missing!\033[0m"
+
                 report.write_tsv(outfile)
-                print(outfile, custom)
+                print(outfile, custom, warning)
+
             except Exception as e:
                 print(outfile + " : report not found : " + e.__class__.__name__, custom)
                 pass
